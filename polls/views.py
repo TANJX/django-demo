e6931c0f71586037c656aca8b17a508cb52caf11
Jianxuan Tan <jianxuat@usc.edu>
2018-10-09 16:25:53 -0700

    generic views

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
@@ -29,6 +30,31 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/result.html', {'question': question})
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Return the last five published questions.
+        return Question.objects.order_by('-pub_date')[:5]
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Polls, 2018'
+        return context
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
 
 
 def vote(request, question_id):
