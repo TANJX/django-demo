0658a89766713adb6c4c78dee775f99b5051aae0
Jianxuan Tan <jianxuat@usc.edu>
2018-10-09 17:05:25 -0700

    test cases

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -38,7 +39,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         # Return the last five published questions.
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
@@ -50,6 +53,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
